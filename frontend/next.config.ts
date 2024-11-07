ab54698cf25d026236856232a544864bb78c761f
Md Al Amin Shaheen <alaminshaheen23@gmail.com>
2024-11-07 15:00:24 -0500

    minor changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -14,19 +14,19 @@ const nextConfig: NextConfig = {
     eslint: {
         ignoreDuringBuilds: false
     },
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Cross-Origin-Opener-Policy",
-                        value: "same-origin" // "same-origin-allow-popups"
-                    }
-                ]
-            }
-        ];
-    }
+    // async headers() {
+    //     return [
+    //         {
+    //             source: "/(.*)",
+    //             headers: [
+    //                 {
+    //                     key: "Cross-Origin-Opener-Policy",
+    //                     value: "same-origin" // "same-origin-allow-popups"
+    //                 }
+    //             ]
+    //         }
+    //     ];
+    // }
 };
 
 export default nextConfig;
