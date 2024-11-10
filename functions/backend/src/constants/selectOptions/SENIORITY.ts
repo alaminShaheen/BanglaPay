2a24e2b86ed28f4cfed4422cae040be3e5b0fa53
Md Al Amin Shaheen <alaminshaheen23@gmail.com>
2024-11-10 15:10:02 -0500

    added junior level to seniority

--- functions/backend/src/constants/selectOptions/SENIORITY.ts
+++ functions/backend/src/constants/selectOptions/SENIORITY.ts
@@ -2,7 +2,8 @@ import { SelectOption } from "@/models/SelectOption";
 import { Seniority } from "@/models/enums/Seniority";
 
 export const SENIORITY: SelectOption<Seniority>[] = [
-    { value: Seniority.INTERN, label: "Entry-level (often ~0-2 years of experience)" },
+    { value: Seniority.INTERN, label: "Entry-level (often <1 years of experience)" },
+    { value: Seniority.JUNIOR, label: "Entry-level (often 1-2 years of experience)" },
     { value: Seniority.MID_LEVEL, label: "Mid-level (often 2-5 years of experience)" },
     { value: Seniority.SENIOR, label: "Senior (often 5-10 years of experience)" },
     { value: Seniority.SENIOR_PLUS, label: "Senior+ (often 10+ years of experience)" },
